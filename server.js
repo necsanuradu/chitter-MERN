c6533f9e4fa410cad6bf31576b5196e315a9669b
Radu Necsanu <radunecsanu@Radus-MacBook-Pro.local>
2021-11-22 11:15:03 +0000

    RN: fix:heroku ready

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
+const path = require('path')// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+});
 
 app.listen(port, () => {
   // perform a database connection when server starts
@@ -16,3 +21,5 @@ app.listen(port, () => {
   });
   console.log(`Server is running on port: ${port}`);
 });
+
+
